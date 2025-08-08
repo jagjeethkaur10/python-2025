a9bbbeb9fc50da3091bbb6ba0f7cb575f12610a7
jagjeethkaur10 <jagjeethkaurjassi@gmail.com>
2025-08-08 17:34:49 +0530

    python basics and github setup

---
+++ 8.8.25.py
@@ -0,0 +1,13 @@
+#python can print numbers directly no need of quatations 
+print(9)
+#we need to put quatations for strings/words , it can be a single quatation/double
+print("hello world")
+print('jassi')
+import keyword
+print(keyword.kwlist)
+from math import pi,sqrt,cbrt
+number=999
+number2=333
+print(pi)
+print(sqrt(number))
+print(cbrt(number2))
