8a92e6b0886658621222d1f799ee45b733b68543
jagjeethkaur10 <jagjeethkaurjassi@gmail.com>
2025-08-08 17:35:47 +0530

    python basics and github setup

--- 8.8.25.py
+++ 8.8.25.py
@@ -3,11 +3,12 @@ print(9)
 #we need to put quatations for strings/words , it can be a single quatation/double
 print("hello world")
 print('jassi')
+# there are 35 keywords in python , they cannot be used as a ordinary identifiers
 import keyword
 print(keyword.kwlist)
 from math import pi,sqrt,cbrt
 number=999
 number2=333
-print(pi)
+print(pi) #pi is a constant
 print(sqrt(number))
 print(cbrt(number2))
